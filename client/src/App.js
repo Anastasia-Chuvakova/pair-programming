c4be189089e8afcc033633807888367996447f34
Anastasia Chuvakova <nansis92@gmail.com>
2020-06-05 14:42:26 -0700

    structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import DrinkName from "./components/DrinkName";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello WORLD</h1>
+      <DrinkName />
+      <Home />
     </div>
   );
 }
