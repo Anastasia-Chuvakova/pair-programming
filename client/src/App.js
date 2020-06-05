b19dcfb981fc981e08c502254be206bdcfa4b332
cory coupe <corycoupe@hotmail.ca>
2020-06-05 15:09:17 -0700

    please work

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import RandomDrink from "./components/RandomDrink";
 function App() {
   return (
     <div className="App">
       <h1>Hello WORLD</h1>
+      <RandomDrink/> 
     </div>
   );
 }
