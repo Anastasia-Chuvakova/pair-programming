5aa7d6e8724f85784ec6cf2bb235cfd4d5ee31b8
anicus19 <anicus19@gmail.com>
2020-06-05 15:10:40 -0700

    css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
+import logo from "./../asset/cocktail-logo.png";
+import { Link } from "react-router-dom";
+import "./../styles/main.css";
 
-export default class Home extends Component {
-  render() {
-    return <div></div>;
-  }
+function Home() {
+  return (
+    <>
+      <div className="body">
+        <header>
+          <div className="header-container">
+            <div className="logo-container">
+              <Link to="/home">
+                <img className="header__logo" src={logo} alt="logo" />
+              </Link>
+            </div>
+            <div className="navbar-container">
+              <div className="search-bar-container">
+                <form className="search-bar">
+                  <input
+                    className="search-bar-input"
+                    type="text"
+                    name="search"
+                    placeholder="Search.."
+                  />{" "}
+                  <button className="button button-upload">
+                    <Link to="/search">
+                      <span>Search</span>
+                    </Link>
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main className="main">
+          <div className="drink-image-container">
+            <img
+              className="drink-image"
+              src="http://place-puppy.com/public-images/index-page/image1/210x280-lg.jpg"
+              alt=""
+            />
+            <button className="random-button">Random</button>
+            <button className="random-button">All Drinks</button>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
+
+export default Home;
